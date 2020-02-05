97d34bb347f599d6a2fb71e7d796b5f31b673873
Raudel Flores <rawdell@outlook.com>
2020-02-04 20:20:12 -0600

    imported createStore, Provider, and the emptyReducer files. Created a 'store' variable that is uses createStore and takes in emptyReducer. Then wrapped the App component with Provider that takes in the store function

--- src/index.js
+++ src/index.js
@@ -2,8 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+// Redux
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+// Reducer
+import { emptyReducer as reducer } from './reducers';
+
+// Styles
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
+// createStore
+const store = createStore(reducer);
+
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, 
+    rootElement);
